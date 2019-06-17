7895454a512e8e7d4ab0a0e1e3c61e80c2c2f067
Yacoub Belaoura <yacoubbelaoura@gmail.com>
2019-06-17 19:11:35 +0100

    fix overflow and padding

--- src/bulma/Dropdown.vue
+++ src/bulma/Dropdown.vue
@@ -10,7 +10,7 @@
                 :class="{ 'is-up top-auto pb-1 pt-0': opensUp }"
                 v-click-outside="close"
                 v-on="dropdownEvents">
-                <div class="dropdown-trigger"
+                <div class="dropdown-trigger w-full"
                     :class="triggerSelector">
                     <slot name="trigger"
                         :triggerEvents="triggerEvents"
@@ -21,7 +21,7 @@
                             whitespace-no-wrap m-0 select-none relative align-top
                             h-10 inline-flex rounded border border-gray-860
                             items-center max-w-full w-full bg-white focus:shadow-outline
-                            focus:border-blue-500 active:border-blue-500  "
+                            focus:border-blue-500 active:border-blue-500 hover:shadow-inner hover:border-gray-480 "
                             type="button"
                             v-on="triggerEvents">
                             <slot name="label"/>
@@ -31,15 +31,16 @@
                 </div>
                 <fade>
                     <!-- <div class="dropdown-menu" -->
-                    <div class="dropdown-menu block right-0 absolute top-full z-10 "
+                    <div class="dropdown-menu block pt-1 right-0 absolute top-full z-10 w-full"
                         :class="dropdownSelector"
                         v-if="visible">
                         <!-- <div class="dropdown-content " -->
-                        <div class="dropdown-content bg-white rounded shadow-md border border-gray-860 flex "
+                        <div class=" bg-white rounded-lg shadow-md border border-gray-860 flex py-2 flex-col overflow-y-auto"
                             @click="attemptClose">
                             <slot name="controls"/>
                             <!-- <div class="options no-scrollbars"> -->
-                            <div class="options no-scrollbars overflow-hidden">
+                            <!-- <div class="options no-scrollbars overflow-hidden w-full "> -->
+                            <div class="options no-scrollbars w-full overflow-y-auto max-h-20 ">
                                 <slot name="options"/>
                             </div>
                         </div>
@@ -75,7 +76,7 @@ export default {
         .dropdown-trigger {
             .button.input {
                 @apply outline-none;
-                min-width: 4em;
+                min-width: 4rem;
                 justify-content: flex-start;
                 line-height: 1.5;
                 padding-bottom: calc(0.375em - 1px);
